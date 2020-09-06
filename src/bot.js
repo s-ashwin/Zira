8bb7a286130b9f2a6b526613b8521e51ee287519
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-09-06 17:07:52 +0530

    Zira can now say hi to the users

--- src/bot.js
+++ src/bot.js
@@ -7,5 +7,12 @@ client.on('ready', ()=>{
     console.log("Hey i'm in");
 })
 
+client.on('message', (message)=>{
+    if(!message.author.bot){
+        if(message.content.toLowerCase()==='hello' || message.content.toLowerCase()==='hi'){
+            message.reply(`Hey ${message.author.username}! This is Zira`);
+        }
+}
+})
 
 client.login(process.env.BOT_TOKEN);
