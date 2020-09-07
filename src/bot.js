b774ffa63a5fcb5bbaece3e548dfc3135111280b
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-09-07 10:00:43 +0530

    Zira can now kick out people

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const{Client} = require('discord.js');
 const client = new Client();
+const prefix = "!";
 
 client.on('ready', ()=>{
     console.log("Hey i'm in");
@@ -10,9 +11,37 @@ client.on('ready', ()=>{
 client.on('message', (message)=>{
     if(!message.author.bot){
         if(message.content.toLowerCase()==='hello' || message.content.toLowerCase()==='hi'){
-            message.reply(`Hey ${message.author.username}! This is Zira`);
+            message.reply(`Hey ${message.author.username}!`);
         }
-}
+        if(message.content.toLowerCase()==='hey zira' || message.content.toLowerCase()==='zira'){
+            message.reply(`Hey ${message.author.username}! This is Zira, How can i help you?`);
+        }
+
+        //COMMANDS
+        if(message.content.startsWith(prefix)){
+            const command = message.content.slice(prefix.length).trim().split(' ').shift();
+
+            //KICK
+            if(command === "kick"){ 
+                if(message.member.hasPermission("KICK_MEMBERS")){
+                    const user = message.mentions.members.first();
+                    if(user){
+                        user.kick()
+                        .then((kickeduser)=>message.channel.send(`${kickeduser} was kicked out of this server`))
+                        .catch((err)=>message.channel.send(`Sorry, I couldn't do that`))
+                    }
+                    else{
+                        message.reply("Please mention a valid user to kick out")
+                    }      
+                    }
+                else{
+                    message.reply(`You don't have permissions to use this command`)
+                }
+            }
+            
+        }
+        }
+        
 })
 
 client.login(process.env.BOT_TOKEN);
