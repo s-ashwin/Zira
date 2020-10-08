a5426998748ab6898546214f48f288d44f32ee58
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-10-08 09:55:08 +0530

    fixed some issues

--- src/bot.js
+++ src/bot.js
@@ -22,10 +22,10 @@ client.on('message', (message)=>{
             message.reply(`Hey ${message.author.username}! This is Zira, I am a Bot`);
         }
 
-          //THANKS
-          if (message.content.toLowerCase().includes("thanks") ||  message.content.toLowerCase().includes("thank")) {
-            message.react('❤️');
-          }
+        //THANKS
+        if (message.content.toLowerCase().includes("thank")) {
+          message.react('❤️');
+        }
 
         //BELIKEBILL
         if (message.content.toLowerCase().startsWith("be like")) {
