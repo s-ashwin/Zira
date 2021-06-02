60c6a63c721cced8a8ae30b97819805f3fafdc50
s-ashwin <ashwin.sivanagarajan@gmail.com>
2021-06-02 19:31:10 +0530

    welcome message

--- src/bot.js
+++ src/bot.js
@@ -44,6 +44,12 @@ client.on('ready', ()=>{
     console.log("Hey i'm in");
 })
 
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 client.on('message', (message)=>{
     if(!message.author.bot){
         //GENERAL
